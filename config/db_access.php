5164a7154ee4a2f6a19656b99e6c22999057aada
div-antk <51283468+owaii@users.noreply.github.com>
2020-03-11 06:53:21 +0900

    mysql test

--- config/db_access.php
+++ config/db_access.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
+$host = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b4ba67588307c0:5aed9396";
+$password = "5aed9396";
 $dbname = "gamelist";
